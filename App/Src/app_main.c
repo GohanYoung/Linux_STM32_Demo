650366b95ca5a12ec50a78cb354b39d5f627584e
lanzhang <17855758540@163.com>
2026-06-29 18:50:10 +0800

    删除蓝牙

--- App/Src/app_main.c
+++ App/Src/app_main.c
@@ -3,118 +3,41 @@
 #include "encoder_motor.h"
 #include "oled.h"
 #include "dht11.h"
-#include "bsp_uart.h"
 #include <stdio.h>
 #include <string.h>
 
-// 全局资源句柄 (类型已经被抽象为 void*)
 BSP_MutexHandle DhtMutex;
-BSP_QueueHandle RpmQueue;
 
 SensorDHT11_t env_sensor = {0};
 int8_t dht11_status = DEV_ERROR;
 
-// ==========================================
-// 业务线程定义
-// ==========================================
+volatile int32_t motor_cycle_count = 0;
+
 void Task_MotorControl(void const * argument) {
-    int16_t new_target_rpm = 0;
     while (1) {
-        // 永远死等队列，哪怕等一万年。只要不来数据，这个线程就不消耗 CPU
-        // 0xFFFFFFFF 代表无限等待 (osWaitForever)
-        if (BSP_Queue_Receive(RpmQueue, &new_target_rpm, 0xFFFFFFFF)) {
-            // 收到蓝牙或按键发来的新指令，更新底层目标值
-            Device_Motor_SetTargetRPM(new_target_rpm);
-        } 
+        motor_cycle_count++;
+        Device_Motor_SetTargetRPM(100);
+        BSP_Sys_Delay(3000);
+        Device_Motor_SetTargetRPM(0);
+        BSP_Sys_Delay(3000);
+        Device_Motor_SetTargetRPM(-100);
+        BSP_Sys_Delay(3000);
+        Device_Motor_SetTargetRPM(0);
+        BSP_Sys_Delay(3000);
     }
 }
-//Device_DHT11_Read中临界区终端无法进入，可能会错过一次电机的PWM纠正
+
 void Task_DHT11(void const * argument) {
     SensorDHT11_t temp_sensor = {0};
     int8_t temp_status;
     while (1) {
         temp_status = Device_DHT11_Read(&temp_sensor); 
-        // 0xFFFFFFFF 相当于无限等待 (WaitForever)
         if (BSP_Mutex_Lock(DhtMutex, 0xFFFFFFFF)) { 
             env_sensor = temp_sensor;
             dht11_status = temp_status;
             BSP_Mutex_Unlock(DhtMutex);
         }
         BSP_Sys_Delay(1500); 
-    }
-}
-
-void Task_Bluetooth(void const * argument) {
-    char cmd_buf[16];
-    uint8_t cmd_idx = 0;
-    uint8_t ch;
-    MotorState_t motor_snapshot;
-    char tx_buf[48];
-    int16_t rpm;
-
-    BSP_Sys_Delay(500);
-    BSP_UART_SendString("READY\r\n");
-
-    while (1) {
-        while (BSP_UART_ReadByte(&ch)) {
-            if (ch == '\r' || ch == '\n') {
-                if (cmd_idx > 0) {
-                    cmd_buf[cmd_idx] = '\0';
-
-                    if (sscanf(cmd_buf, "FW %hd", &rpm) == 1) {
-                        BSP_Queue_Send(RpmQueue, &rpm, 0);
-                        snprintf(tx_buf, sizeof(tx_buf), "OK FW=%d\r\n", rpm);
-                        BSP_UART_SendString(tx_buf);
-                    }
-                    else if (sscanf(cmd_buf, "fw %hd", &rpm) == 1) {
-                        BSP_Queue_Send(RpmQueue, &rpm, 0);
-                        snprintf(tx_buf, sizeof(tx_buf), "OK FW=%d\r\n", rpm);
-                        BSP_UART_SendString(tx_buf);
-                    }
-                    else if (sscanf(cmd_buf, "RV %hd", &rpm) == 1) {
-                        rpm = -rpm;
-                        BSP_Queue_Send(RpmQueue, &rpm, 0);
-                        snprintf(tx_buf, sizeof(tx_buf), "OK RV=%d\r\n", -rpm);
-                        BSP_UART_SendString(tx_buf);
-                    }
-                    else if (sscanf(cmd_buf, "rv %hd", &rpm) == 1) {
-                        rpm = -rpm;
-                        BSP_Queue_Send(RpmQueue, &rpm, 0);
-                        snprintf(tx_buf, sizeof(tx_buf), "OK RV=%d\r\n", -rpm);
-                        BSP_UART_SendString(tx_buf);
-                    }
-                    else if (strcmp(cmd_buf, "STOP") == 0 || strcmp(cmd_buf, "stop") == 0) {
-                        rpm = 0;
-                        BSP_Queue_Send(RpmQueue, &rpm, 0);
-                        BSP_UART_SendString("STOP\r\n");
-                    }
-                    else if (strcmp(cmd_buf, "STATUS") == 0 || strcmp(cmd_buf, "status") == 0) {
-                        Device_Motor_GetState(&motor_snapshot);
-                        int8_t temp_status;
-                        if (BSP_Mutex_Lock(DhtMutex, 10)) {
-                            temp_status = dht11_status;
-                            BSP_Mutex_Unlock(DhtMutex);
-                        } else {
-                            temp_status = DEV_ERROR;
-                        }
-                        snprintf(tx_buf, sizeof(tx_buf),
-                                 "T:%d R:%d P:%d D:%d\r\n",
-                                 motor_snapshot.target_rpm,
-                                 motor_snapshot.real_rpm,
-                                 motor_snapshot.current_pwm,
-                                 temp_status);
-                        BSP_UART_SendString(tx_buf);
-                    }
-                    else {
-                        BSP_UART_SendString("ERR\r\n");
-                    }
-                    cmd_idx = 0;
-                }
-            } else if (cmd_idx < sizeof(cmd_buf) - 1) {
-                cmd_buf[cmd_idx++] = ch;
-            }
-        }
-        BSP_Sys_Delay(50);
     }
 }
 
@@ -137,11 +60,9 @@ void Task_OLED(void const * argument) {
             BSP_Mutex_Unlock(DhtMutex);
         }
 
-        // 第一行：目标转速 & 实际转速
         snprintf(display_buf, sizeof(display_buf), "T:%4d R:%4d", tar_rpm, cur_rpm);
         Device_OLED_ShowString(0, 0, display_buf);
 
-        // 第二行：PWM & 温湿度（DHT11 离线则显示 "--"）
         if (local_status == DEV_OK) {
             snprintf(display_buf, sizeof(display_buf), "P:%4d T:%02d H:%02d",
                      cur_pwm, local_sensor.temperature / 10, local_sensor.humidity / 10);
@@ -149,33 +70,27 @@ void Task_OLED(void const * argument) {
             snprintf(display_buf, sizeof(display_buf), "P:%4d T:-- H:--", cur_pwm);
         }
         Device_OLED_ShowString(0, 2, display_buf);
+
+        snprintf(display_buf, sizeof(display_buf), "CYC:%ld", motor_cycle_count);
+        Device_OLED_ShowString(0, 4, display_buf);
         
         BSP_Sys_Delay(200); 
     }
 }
 
-// ==========================================
-// 启动入口
-// ==========================================
 void App_Main_Run(void) {
     BSP_HAL_Delay(200); 
-    BSP_Delay_us_Init(); //开启DWT(微秒)
-    BSP_UART_Init(); 
+    BSP_Delay_us_Init();
     Device_OLED_Init();
     Device_OLED_Clear();
-    Device_Motor_Init();    //已经将控制函数挂载钩子上
+    Device_Motor_Init();
     Device_DHT11_Init();
     Device_Motor_SetTargetRPM(0); 
-    //开启中断TIM4
     BSP_Sys_TIM4_Start();
 
-    // 创建纯净版 IPC 资源
     DhtMutex = BSP_Mutex_Create();
-    RpmQueue = BSP_Queue_Create(4, sizeof(int16_t));
 
-    // 创建纯净版线程 (优先级数字越大优先级越高，具体视 OS 映射而定)
     BSP_Thread_Create(Task_MotorControl, "Motor", 128, 4);
-    BSP_Thread_Create(Task_Bluetooth,    "BT",    256, 3);
     BSP_Thread_Create(Task_OLED,         "OLED",  256, 2);
     BSP_Thread_Create(Task_DHT11,        "DHT",   128, 1);
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,6 @@
 #include "cmsis_os.h"
 #include "i2c.h"
 #include "tim.h"
-#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -96,7 +95,6 @@ int main(void)
   MX_TIM2_Init();
   MX_TIM3_Init();
   MX_TIM4_Init();
-  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   App_Main_Run();
   /* USER CODE END 2 */
